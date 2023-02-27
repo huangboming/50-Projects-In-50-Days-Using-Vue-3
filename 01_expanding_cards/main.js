a67d4ac908e5cdb04c6e004f80fb3b0eb677b781
Boming <48573436+huangboming@users.noreply.github.com>
2023-02-27 15:29:00 +0800

    initialize repo

---
+++ 01_expanding_cards/main.js
@@ -0,0 +1,47 @@
+const panel = {
+    props: ['title', 'url'],
+    template: `
+    <div class="panel" :style="{ 'background-image': 'url(' + url + ')' }">
+        <h3>
+            {{ title }}
+        </h3>
+    </div>
+    `
+}
+
+const App = {
+    data() {
+        return {
+            panelTitles: Seed.panelTitles,
+            panelUrls: Seed.panelUrls,
+            activePanel: 0,
+        }
+    },
+    components: {
+        'panel': panel
+    },
+    methods: {
+        range(num) {
+            return [...Array(num).keys()];
+        },
+        changeActivePanel(index) {
+            this.activePanel = index;
+        }
+    },
+    template: `
+    <div class='container'>
+        <panel
+        v-for="index in range(panelUrls.length)"
+        :class="{'active-panel': index === activePanel}" 
+        @click="changeActivePanel(index)"
+        :title="panelTitles[index]"
+        :url="panelUrls[index]"
+        />
+    </div>
+    `,
+    mounted() {
+
+    }
+}
+
+Vue.createApp(App).mount("#app");
---
+++ README.md
@@ -0,0 +1,9 @@
+# 50-Projects-In-50-Days-Using-Vue-3
+
+(Part of my frontend exercise)
+
+50 small projects, written in Vue 3.
+
+Original project: https://github.com/bradtraversy/50projects50days
+Udemy course: https://www.udemy.com/course/50-projects-50-days/
+
