0f23162684b911a762ae0b7d7f27c4ca4bda6b37
Boming <48573436+huangboming@users.noreply.github.com>
2023-03-09 19:12:04 +0800

    add project 8

---
+++ 08_form_wave/main.js
@@ -0,0 +1,49 @@
+const Wave = {
+    template: `
+        <label :for="contents">
+            <span v-for="char, i in contents" :style="{'transitionDelay': i * 50 + 'ms'}">
+                {{ char }}
+            </span>
+        </label >
+    `,
+    props: ['contents',]
+}
+
+const App = {
+    components: {
+        Wave,
+    },
+    template: `
+    <div class="container">
+        <h1>Please Login</h1>
+        <form action="" @submit.prevent="handleSubmit">
+            <div class="form-control">
+                <input type="text" v-model="email" required>
+                <Wave contents="Email" />
+            </div>
+            <div class="form-control">
+                <input type="password" v-model="password" required>
+                <Wave contents="Password" />
+            </div>
+            <button class="btn">Login</button>
+            <p>Don't have an account? <a href="#">Register</a></p>
+        </form>
+    </div>
+    `,
+    data() {
+        return {
+            email: null,
+            password: null,
+        }
+    },
+    methods: {
+        handleSubmit() {
+            const msg = `Your mail is ${this.email}\nYour password is ${this.password}`;
+            // console.log(this.email)
+            // console.log(this.password)
+            alert(msg)
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
