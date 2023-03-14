7ba8b673566e9e11285d247ebb6719504c1024bc
Boming <48573436+huangboming@users.noreply.github.com>
2023-03-14 20:13:44 +0800

    add project 11

---
+++ 11_event_keycodes/main.js
@@ -0,0 +1,34 @@
+const App = {
+    template: `
+    <div class="container">
+        <div class="key" v-if="!isPressed">
+            Press any key to get the keyCode
+        </div>
+        <template v-else>
+            <div class="key" v-for="key, i in keyArray">
+                {{ computedKeyArray[i] }}
+                <small>{{ key }}</small>
+            </div>
+        </template>
+    </div>
+    `,
+    data() {
+        return {
+            isPressed: false,
+            keyArray: ['event.key', 'event.keyCode', 'event.code'],
+            computedKeyArray: [],
+        }
+    },
+    methods: {
+        updateKeyCode(event) {
+            this.isPressed = true;
+            this.computedKeyArray = this.keyArray.map((key) => eval(key) == ' ' ? 'Space' : eval(key))
+            console.log(this.computedKeyArray)
+        }
+    },
+    mounted() {
+        window.addEventListener('keydown', this.updateKeyCode);
+    }
+}
+
+Vue.createApp(App).mount('#app')
