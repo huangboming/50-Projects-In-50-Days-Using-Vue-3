63a07f664e207282bc80c01dc7811c69b86d250e
Boming <48573436+huangboming@users.noreply.github.com>
2023-03-21 13:19:22 +0800

    add project 18

---
+++ 18_background_slider/main.js
@@ -0,0 +1,64 @@
+const Slide = {
+    props: ["isActive", "bgUrl"],
+    template: `
+    <div 
+      class="slide"
+      :class="isActive ? 'active' : ''"
+      :style="{ 'background-image': 'url(' + bgUrl +')' }">
+    </div>
+    `
+}
+
+const App = {
+    components: {
+        Slide,
+    },
+    template: `
+    <div class="slider-container">
+        <Slide 
+        v-for="imgUrl, i in imgUrls"
+        :is-active="activeImgIdx === i" 
+        :bgUrl="imgUrl" />
+    
+        <button class="arrow left-arrow" id="left" @click="decrease">
+            <i class="fas fa-arrow-left"></i>
+        </button>
+
+        <button class="arrow right-arrow" id="right" @click="increase">
+            <i class="fas fa-arrow-right"></i>
+        </button>
+    </div>
+    `,
+    data() {
+        return {
+            imgUrls: Seed.imgUrls,
+            activeImgIdx: 0
+        }
+    },
+    methods: {
+        setBodyBg() {
+            document.body.style.backgroundImage =  `url(${this.imgUrls[this.activeImgIdx]})`;
+        },
+        decrease() {
+            if (this.activeImgIdx > 0) {
+                this.activeImgIdx--;
+            } else {
+                this.activeImgIdx = this.imgUrls.length - 1;
+            }
+            this.setBodyBg();
+        },
+        increase() {
+            if (this.activeImgIdx < this.imgUrls.length - 1) {
+                this.activeImgIdx++;
+            } else {
+                this.activeImgIdx = 0;
+            }
+            this.setBodyBg();
+        }
+    },
+    mounted() {
+        this.setBodyBg();
+    }
+}
+
+Vue.createApp(App).mount('#app');
