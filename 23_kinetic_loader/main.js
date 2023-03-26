9cccf35eec078273ab6a0b72e714544f795c106c
Boming <48573436+huangboming@users.noreply.github.com>
2023-03-26 11:24:15 +0800

    add project 23

---
+++ 23_kinetic_loader/main.js
@@ -0,0 +1,10 @@
+const App = {
+    template: `
+    <div class="container">
+        <div class="triangle" id="triangle1"></div>
+        <div class="triangle" id="triangle2"></div>
+    </div>
+    `
+}
+
+Vue.createApp(App).mount('#app')
